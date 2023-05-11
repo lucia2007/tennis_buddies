b394c9d50cf8b30e032126de4b6cb4cdf777153a
Lucia Ferencik <lferencikova@gmail.com>
2023-05-11 12:17:06 +0000

    Add admin model for Buddy app

--- buddies/admin.py
+++ buddies/admin.py
@@ -1,4 +1,24 @@
-from django.contrib import admin
-# from .models import Buddies
+from django.contrib import admin  # type: ignore
+from .models import UserProfile, Buddy
+from django_summernote.admin import SummernoteModelAdmin   # type: ignore
 
-# Register your models here.
+
+@admin.register(Buddy)
+class BuddyAdmin(SummernoteModelAdmin):
+
+    list_display = ('get_user_name', 'status', 'gender', 'level', 'game_type', 'availability', 'practice_type', 'created_on')
+
+    @admin.display(ordering='user_profile__first_name', description='name')
+    def get_user_name(self, obj):
+        return obj.user_profile.first_name + " " + obj.user_profile.last_name
+
+    search_fields = ['user_profile', 'about_me']
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('about_me')
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'created_on')
+    list_filter = ('first_name', 'created_on')
+    search_fields = ['first_nane', 'last_name', 'email', 'phone']
