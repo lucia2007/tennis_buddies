859280cd29553c60fc2a0c7a47233c7e8f8bea24
Lucia Ferencik <lferencikova@gmail.com>
2023-05-17 12:01:11 +0000

    Fix mypy/stubs errors (mypy.ini)

--- buddies/models.py
+++ buddies/models.py
@@ -1,10 +1,10 @@
-from django.db import models  # type: ignore
+from django.db import models
 # Import User model from the Django auth app
-from django.contrib.auth.models import User  # type: ignore
+from django.contrib.auth.models import User
 # Import Cloudinary for storing images
 from cloudinary.models import CloudinaryField  # type: ignore
 from datetime import date
-from django.core.validators import RegexValidator  # type: ignore
+from django.core.validators import RegexValidator
 
 
 class UserProfile(models.Model):
@@ -32,22 +32,32 @@ class Buddy(models.Model):
         UserProfile, related_name="profile_owner", on_delete=models.CASCADE)
     about_me = models.TextField()  # make it into RichTextField
     data_of_birth = models.DateField()
-    profile_picture = CloudinaryField('image', default='placeholder')  # change placeholder text
+    # change placeholder text
+    profile_picture = CloudinaryField('image', default='placeholder')  
     picture_description = models.CharField(max_length=200)
     # add picture description
     # excerpt = models.TextField(blank=True)
     is_approved = models.BooleanField(default=False)
     status = models.IntegerField(choices=STATUS, default=1)
     gender = models.CharField(max_length=6, choices=[
-                              ('M', 'Male'), ('F', 'Female')])
+                              ('M', 'Male'),
+                              ('F', 'Female')])
     level = models.CharField(max_length=15, choices=[
-                              ('Beginner', 'Beginner'), ('Intermediate', 'Intermediate'), ('Advanced', 'Advanced')])
+                              ('Beginner', 'Beginner'), 
+                              ('Intermediate', 'Intermediate'),
+                              ('Advanced', 'Advanced')])
     practice_type = models.CharField(max_length=25, choices=[
-                              ('Hitting Practice', 'Hitting Practice'), ('Match Practice', 'Match Practice'), ('Both', 'Both')])
+                              ('Hitting Practice', 'Hitting Practice'),
+                              ('Match Practice', 'Match Practice'),
+                              ('Both', 'Both')])
     game_type = models.CharField(max_length=10, choices=[
-                              ('Singles', 'Singles'), ('Doubles', 'Doubles'), ('Both', 'Both')])
+                              ('Singles', 'Singles'), 
+                              ('Doubles', 'Doubles'),
+                              ('Both', 'Both')])
     availability = models.CharField(max_length=10, choices=[
-                              ('Morning', 'Morning'), ('Afternoon', 'Afternoon'), ('Both', 'Both')])
+                              ('Morning', 'Morning'),
+                              ('Afternoon', 'Afternoon'),
+                              ('Both', 'Both')])
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
