f0f7ff70b005a07601f810fbc343070aba71ffee
Lucia Ferencik <lferencikova@gmail.com>
2023-05-29 11:59:48 +0000

    Undo nullable

--- profiles/models.py
+++ profiles/models.py
@@ -12,9 +12,7 @@ class UserProfile(models.Model):
         User, related_name="user_profile", on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    phone = models.CharField(
-        max_length=20,
-        null=True)
+    phone = models.CharField(max_length=20)
     # validators=[RegexValidator(r'^\+?\d{9, 15}$')])  # validate if phone#
     created_on = models.DateTimeField(auto_now=True)
 
