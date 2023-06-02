d222b706b5b8ee55ab17ed669a1062638d4a221c
Lucia Ferencik <lferencikova@gmail.com>
2023-06-02 13:01:49 +0000

    Finalize Booking list view

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import AddBooking, Bookings, BookingDetail
+from .views import AddBooking, Bookings, BookingDetail, DeleteBooking
 from . import views
 
 urlpatterns = [
     path("", views.booking, name="booking"),
     path('add/', AddBooking.as_view(), name='add-booking'),
-    path('bookings/', Bookings.as_view(), name='bookings'),
+    path('list/', Bookings.as_view(), name='list-bookings'),
     path('<slug:pk>/', BookingDetail.as_view(), name='booking-detail'),
+    path('delete/<slug:pk>/', DeleteBooking.as_view(), name='delete-booking'),
 ]
