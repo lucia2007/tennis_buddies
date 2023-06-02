088bed5865f233ec00542c085b8ba66a7ba12e6c
Lucia Ferencik <lferencikova@gmail.com>
2023-06-02 10:55:09 +0000

    Bug fix: change the order of urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.booking, name="booking"),
     path('add/', AddBooking.as_view(), name='add-booking'),
-    path('<slug:pk>/', BookingDetail.as_view(), name='booking-detail'),
     path('bookings/', Bookings.as_view(), name='bookings'),
+    path('<slug:pk>/', BookingDetail.as_view(), name='booking-detail'),
 ]
