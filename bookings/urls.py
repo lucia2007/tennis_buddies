138fe382d985599d8e794395024944d2b9625277
Lucia Ferencik <lferencikova@gmail.com>
2023-06-06 11:07:02 +0000

    all_or_own url pattern for Bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('add/', AddBooking.as_view(), name='add-booking'),
     # I got a tip from my husband to use the regular expression
     # to alternate ListViews depending on url pattern
+    # https://docs.djangoproject.com/en/4.2/topics/db/queries/
     re_path('list/(?P<all_or_own>all|own)/', BookingListView.as_view(), name='list-bookings'),
     path('<slug:pk>/', BookingDetail.as_view(), name='booking-detail'),
     path('delete/<slug:pk>/', DeleteBooking.as_view(), name='delete-booking'),
