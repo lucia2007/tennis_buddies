09629896469e8ef80f50699e7e86b7ce8453d1d4
Lucia Ferencik <lferencikova@gmail.com>
2023-06-07 11:52:34 +0000

    Restrict #of opponents + owner=current user

--- bookings/forms.py
+++ bookings/forms.py
@@ -4,10 +4,18 @@ from .models import Booking, Event
 
 class BookingForm(forms.ModelForm):
     """ A form to create a booking """
+
+    # Override clean method in order to restrict the max amount of opponents
+    # https://stackoverflow.com/questions/20203806/limit-maximum-choices-of-manytomanyfield/20230270#20230270
+    def clean_opponents(self):
+        opponents = self.cleaned_data['opponents']
+        if len(opponents) > 3:
+            raise forms.ValidationError('You can add maximum 3 opponents')
+        return opponents
+
     class Meta:
         model = Booking
         fields = [
-            'owner',
             'date',
             'time',
             'court',
