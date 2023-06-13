cfdd82080147c18b7e1bfa3d0807a0721dd85195
Lucia Ferencik <lferencikova@gmail.com>
2023-06-13 13:20:38 +0000

    Populate contact.html (contact form + map)

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import *
-from django.contrib import messages
+# from django.contrib import messages
+# to display messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
 
 
 def contact(request):
-    return render(request, "contact/contact.html", {})
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        # send an email
+        send_mail (
+            'message-from ' + message_name,  # Subject
+            message,  # Message
+            message_email,  # Email from
+            ['lferencikova@gmail.com'],  # Email to
+
+        )
+
+
+        return render(request, "contact/contact.html", {'message-name': message_name})
+    else:
+        return render(request, "contact/contact.html", {})
